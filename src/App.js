fb177de3b26917e2e490b84a988fa8036977695b
MANOJ KUMAR SINGH <manojkumarsingh@MANOJs-MacBook-Air.local>
2023-10-21 23:47:12 +0530

    po

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import React from 'react';
+import './App.css';
+import AllRoutes from './components/AllRoutes';
+import Navbar from './components/Navbar';
 
-import { About, Footer, Header, Skills, Testimonial, Work, GithubStats } from "./container";
-import { Navbar } from './components';
-import "./App.scss";
+import './components/Global.scss'
 
-const App = () => {
+function App() {
   return (
-    <div className='app'>
-      <Navbar />
-      <Header />
-      <About />
-      <Work />
-      <Skills />
-      <Testimonial />
-      <GithubStats />
-      <Footer />
-      <div class="app__flex" style={{ paddingTop: "1rem", paddingBottom: "1rem", flexDirection: "column", gap: "0.5rem" }}>
-        <p class="footer-text">
-          Designed & Built by
-        </p>
-        <p class="footer-text" style={{ color: "#313bac", fontWeight: "500" }}>
-          Kopparapu Ravitheja
-        </p>
-      </div>
+    <div>
+      <Navbar/>
+      <AllRoutes/>
     </div>
   );
 }
