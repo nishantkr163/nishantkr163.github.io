10db1578c4f336502923fbaf4b599453d1ae4bbe
MANOJ KUMAR SINGH <manojkumarsingh@MANOJs-MacBook-Air.local>
2023-10-22 15:21:44 +0530

    contact section with emailjs

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from './Input.module.css';
 
-const Input = ({innerText, type}) => {
+const Input = ({innerText, type, name, value, handleInput}) => {
   return (
     <div className={styles.form__group}>
       <input
+        onChange={(e) => handleInput(e)}
+        value={value}
+        name={name}
         type={type}
         className={styles.form__field}
         placeholder="Email"
